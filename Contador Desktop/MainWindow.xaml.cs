87f336243e0720c15ec700132c1129b8e227e232
GRECIA-LIBERTAD-PISTE-PEREZ <19470356@itcampeche.edu.mx>
2020-10-14 18:47:20 -0500

    ContadorDesktop: Agrega el manejo de eventos

--- Contador Desktop/MainWindow.xaml.cs
+++ Contador Desktop/MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace Contador_Desktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+private int conteo;
+
         public MainWindow()
         {
             InitializeComponent();
+            conteo = 0;
+        }
+        private void contarButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Bueno aquí está de mas dar a entender que estos algoritmos son para que tengan funcionalidad los botones, contar//
+            //reiniciar y salir//
+
+            conteo++;
+            conteoLabel.Content = Convert.ToString(conteo);
+        }
+        //el object sender se genera automaticamente con el evento, lo que hace el sender es que convierte un objeto en tipo botón//
+
+        private void reiniciarButton_Click(object sender, RoutedEventArgs e)
+        {
+            conteo = 0;
+            conteoLabel.Content = Convert.ToString(conteo);
+        }
+        private void salirButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
+       
+        
+    
 }
